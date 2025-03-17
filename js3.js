6b1e016e8a0d2fd8bb8edf35313daba8822e75d7
martin6101 <martin6101@gmail.com>
2025-03-17 16:40:15 +0100

    Update js3.js
    Buitenlander

--- js3.js
+++ js3.js
@@ -10,12 +10,7 @@ define(['pipAPI', 'https://martin6101.github.io/IAT/fullscript.js'], function(AP
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
 		    media : [ //Stimuli content as PIP's media objects
-    		    	{word : 'Immigrant Group'}, 
-    			{word : 'Immigrant Group'}, 
-    			{word : 'Immigrant Group'}, 
-    			{word : 'Immigrant Group'}, 
-    			{word : 'Immigrant Group'}, 
-    			{word : 'Immigrant Group'}
+    		    	{word : 'Buitenlander'}    	
 		    ], 
 		    //Stimulus css (style)
 		    css : {color:'#000000','font-size':'3em'}
@@ -26,7 +21,7 @@ define(['pipAPI', 'https://martin6101.github.io/IAT/fullscript.js'], function(AP
 			name : 'Unpleasant', //Attribute label
 			title : {
 				media : {word : 'Negative'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				css : {color:'#000000','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
@@ -38,14 +33,14 @@ define(['pipAPI', 'https://martin6101.github.io/IAT/fullscript.js'], function(AP
 				{word: 'Grief'}
 			], 
 			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
+			css : {color:'#000000','font-size':'3em'}
 			},
 		attribute2 : 
 			{
 			name : 'Pleasant', //Attribute label
 			title : {
 				media : {word : 'Positive'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				css : {color:'#000000','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
@@ -57,7 +52,7 @@ define(['pipAPI', 'https://martin6101.github.io/IAT/fullscript.js'], function(AP
 				{word: 'Love'}
 			], 
 			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
+			css : {color:'#000000','font-size':'3em'}
 			},
 
   base_url : {//Where are your images at?
