93af2b395f0f832ab944a8ed1dada22a8faa8e65
martin6101 <martin6101@gmail.com>
2025-03-21 10:51:10 +0100

    Center

--- fullscript.js
+++ fullscript.js
@@ -19,7 +19,11 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 					'height': '600px', 
 					'border-width': '3px',  // Normal border
 					'border-color': '#525252',  // Gray border //For black: #000000
-					'border-style': 'solid' // Ensure the border is visible
+					'border-style': 'solid',
+					'display': 'flex',               // Enable flexbox
+					'justify-content': 'center',      // Center horizontally
+					'align-items': 'center',          // Center vertically
+					'text-align': 'center' // Ensure the border is visible
 				}
 			}, 
 			//Define the category.
